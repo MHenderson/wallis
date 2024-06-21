829db4187ea65d394cc0067a7472b2651c9681ce
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-06-21 06:55:04 +0000

    Add empty_cells and unused_pairs functions.

---
+++ R/unused-pairs.R
@@ -0,0 +1,24 @@
+#' Pairs not used in a partial Room square
+#'
+#' @param R A partial Room square.
+#'
+#' @return A list of pairs not used in R.
+#' @export
+unused_pairs <- function(R) {
+  
+  n <- max(R$row) + 1
+
+  used_pairs <- R |> dplyr::select(first, second)
+  
+  x <- combn(0:(n - 1), 2)
+  
+  all_pairs <- tibble::tibble(
+     first = x[1,],
+    second = x[2,]
+  )
+  
+  dplyr::anti_join(all_pairs, used_pairs, by = c("first", "second")) |>
+    dplyr::mutate(ffs = purrr::map2(first, second, c)) |>
+    dplyr::pull(ffs)
+
+}
